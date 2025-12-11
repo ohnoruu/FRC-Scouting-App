20a61e7b329dba4b67cd879216f6687609f67602
RubyD <143292762+ohnoruu@users.noreply.github.com>
2025-12-11 20:53:23 +0000

    Update Profile

--- scouting/src/navigation/search-nav/Profile.jsx
+++ scouting/src/navigation/search-nav/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { Container, Image, Tab, Tabs, ListGroup, Card } from 'react-bootstrap';
 import { FaArrowLeft } from 'react-icons/fa';
 import fillerImg from '../../assets/interface-icons/filler-image.png';
 import axios from 'axios';
@@ -38,81 +39,79 @@ export default function Profile() {
     };
 
     return (
-        <div className="profile_container">
-            <div className="profile_topPiece" />
-            <FaArrowLeft onClick={() => navigate(-1)} className="profile_buttonPiece" />
-            <div className="profile_middlePieceContainer">
-                <div className="profile_middlePiece">
+        <Container className="profile_container" fluid="md">
+            <FaArrowLeft onClick={() => navigate(-1)} className="profile_backButton"/>
+            {robotProfileData ? (
+                <div className="profile_content">
+                    <div className="profile_header">
+                        <div className="profile_headerText">
+                            <h1>{robotProfileData.profile?.teamName}</h1>
+                            <h2>Team {robotProfileData.profile?.teamNumber}</h2>
+                        </div>
+                        <Image
+                            src={fillerImg}
+                            alt="Robot"
+                            className="profile_img"
+                            thumbnail
+                        />
+                    </div>
 
-                {robotProfileData ? (
-                    <>
-                        <div className="profile_teamMain">
-                            <div className="profile_teamSubMain">
-                                <span className="profile_header">{robotProfileData.profile?.teamName}</span>
-                                <span className="profile_header">{robotProfileData.profile?.teamNumber}</span>
-                            </div>
-
-                            <img className="profile_image" src={fillerImg} alt="Robot" />
+                <Tabs 
+                    style={{marginTop: '2rem'}}
+                    defaultActiveKey="robot"
+                    id="profile_tabs"
+                    className="mb-3"
+                >
+                    <Tab eventKey="robot" title="Robot">
+                        <div className="profile_section">
+                            <h2>Robot Details</h2>
+                            <ListGroup>
+                                <ListGroup.Item>Drivebase: {robotProfileData.profile?.drivebase}</ListGroup.Item>
+                                <ListGroup.Item>Weight:</ListGroup.Item>
+                                <ListGroup.Item>Height:</ListGroup.Item>
+                                <ListGroup.Item>
+                                    Playstyles: {[
+                                        robotProfileData.profile?.playstyle?.algae && "Algae Scorer",
+                                        robotProfileData.profile?.playstyle?.coral && "Coral Scorer", 
+                                        robotProfileData.profile?.playstyle?.defense && "Defender"
+                                    ].filter(Boolean).join(", " ) || "None"}
+                                </ListGroup.Item>
+                                <ListGroup.Item>Autonomous Details: {robotProfileData.profile?.autoDetails || 'N/A'}</ListGroup.Item>
+                                <ListGroup.Item>Climbing:
+                                    {robotProfileData.profile?.climbing?.shallow && ' Can climb shallow cage'} 
+                                    {robotProfileData.profile?.climbing?.shallow && robotProfileData.profile?.climbing?.deep ? ' and ' : ''}
+                                    {robotProfileData.profile?.climbing?.deep && 'Can climb deep cage'}
+                                    {!robotProfileData.profile?.climbing?.shallow && !robotProfileData.profile?.climbing?.deep && 'None'}
+                                </ListGroup.Item>
+                                <ListGroup.Item>Algae Intake: {Object.entries(robotProfileData.profile?.intakeData.algae || {}).map(([key, value]) => value ? key : null).filter(Boolean).join(', ')}{robotProfileData.profile?.intakeData.algae?.other ? ` (${robotProfileData.profile.intakeData.algae.other})` : '' || ''}</ListGroup.Item>
+                                <ListGroup.Item>Coral Intake: {Object.entries(robotProfileData.profile?.intakeData.coral || {}).map(([key, value]) => value ? key : null).filter(Boolean).join(', ')}{robotProfileData.profile?.intakeData.coral?.other ? ` (${robotProfileData.profile.intakeData.coral.other})` : '' || ''}</ListGroup.Item>
+                            </ListGroup>
                         </div>
 
-                        <div className="profile_robotDetails">
-                            <span className="profile_header">General Details</span>
-                            <span className="profile_text">Drivebase: {robotProfileData.profile?.drivebase}</span>
-                            <span className="profile_text">Playstyles: {[
-                                    robotProfileData.profile?.playstyle?.algae && "Algae Scorer",
-                                    robotProfileData.profile?.playstyle?.coral && "Coral Scorer", 
-                                    robotProfileData.profile?.playstyle?.defense && "Defender"
-                                ].filter(Boolean).join(", " ) || "None"}
-                            </span>
-                            <span className="profile_text">Autonomous Details: {robotProfileData.profile?.autoDetails || 'N/A' /*retest this, post request didnt go through first time*/ }</span>  
-                            <span className="profile_text">Climbing Capabilities: 
-                                {robotProfileData.profile?.climbing?.shallow && ' Can climb shallow cage'} 
-                                {robotProfileData.profile?.climbing?.shallow && robotProfileData.profile?.climbing?.deep ? ' and ' : ''}
-                                {robotProfileData.profile?.climbing?.deep && 'Can climb deep cage'}
-                                {!robotProfileData.profile?.climbing?.shallow && !robotProfileData.profile?.climbing?.deep && 'None'}
-                            </span>
-                            <span className="profile_text">Additional Details: {robotProfileData.profile?.additionalDetails || 'N/A'}</span>
-
-                            <div className="profile_intakeSection">
-                                <span className="profile_header">Intake Capabilities</span>
-                                <span className="profile_text">Algae: {Object.entries(robotProfileData.profile?.intakeData.algae || {}).map(([key, value]) => value ? key : null).filter(Boolean).join(', ')}{robotProfileData.profile?.intakeData.algae?.other ? ` (${robotProfileData.profile.intakeData.algae.other})` : '' || ''}</span>
-                                <span className="profile_text">Coral: {Object.entries(robotProfileData.profile?.intakeData.coral || {}).map(([key, value]) => value ? key : null).filter(Boolean).join(', ')}{robotProfileData.profile?.intakeData.coral?.other ? ` (${robotProfileData.profile.intakeData.coral.other})` : '' || ''}</span>
-                            </div>
-
-                            <div className="profile_scoringSection">
-                                <span className="profile_header">Scoring Capabilities</span>
-                                <span className="profile_text">Algae: Net - {computeScore(robotProfileData.profile?.scoreCapability.algae.netScoring)}, Processor - {computeScore(robotProfileData.profile?.scoreCapability.algae.processorScoring)}</span>
-                                <span className="profile_text">
-                                    Coral: {Object.entries(robotProfileData.profile?.scoreCapability.coral || {})
-                                        .filter(([level, value]) => value !== undefined)
-                                        .map(([level, value]) => `${level}: ${computeScore(value)}`)
-                                        .join(', ') || 'None'}
-                                </span>
-                                <span className="profile_text">Autonomous: {computeScore(robotProfileData.profile?.scoreCapability.autoCapability)}</span>
-                            </div>
+                        <div className="profile_section">
+                            <h2>Scoring</h2>
+                            <ListGroup>
+                                
+                            </ListGroup>
+                            <p>Algae</p>
+                            <ListGroup>
+                                <ListGroup.Item>Algae (Net) - {computeScore(robotProfileData.profile?.scoreCapability.algae.netScoring)}</ListGroup.Item>
+                                <ListGroup.Item>Algae (Processor) - {computeScore(robotProfileData.profile?.scoreCapability.algae.processorScoring)}</ListGroup.Item>
+                            </ListGroup>
                         </div>
-                    </>
-                ) : (
-                    <span>Loading...</span>
-                )}
+                    </Tab>
+                    <Tab eventKey="match" title="Matches">
 
-                <div className="profile_matchSection">
-                    <span className="profile_header">Match History</span>
-                    {robotProfileData?.matches?.map((match) => (
-                        <div key={`${robotProfileData.profile?.teamName} match ${match.matchNumber}`}
-                            onClick={() => navigate('/navigator/search/match-stats', { state: { teamNumber: robotProfileData.profile?.teamNumber, matchData: match } })}>
+                    </Tab>
+                    <Tab eventKey="photos" title="Photos">
 
-                            <div className="profile_matches">
-                                <span className="profile_text"> Match Number: {match.matchNumber} </span>
-                                <span className="profile_text"> Match Type: {match.matchType} </span>
-                                <span className="profile_text"> Raw Score: {match.score}</span>
-                            </div>
+                    </Tab>
+                </Tabs>
 
-                        </div>
-                    ))}
                 </div>
-                </div>
-            </div>
-        </div>
+            ) : (
+                <h1>Loading...</h1>
+            )}
+        </Container>
     );
 }
